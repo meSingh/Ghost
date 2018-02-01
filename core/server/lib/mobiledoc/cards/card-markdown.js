meSingh/Ghost

commit 2add938e9642110b92c94542e6b0538c45ae8242

Koenig - Rename server-side cards
refs https://github.com/TryGhost/Ghost/issues/9311
- match card names to the new generic Koenig card names introduced in https://github.com/TryGhost/Ghost-Admin/commit/95a068615db20664dab578da2e71499ce8b9c50d

core/server/lib/mobiledoc/cards/card-markdown.js
@@ -1,0 +1,4 @@
+// this card is just an alias of the `markdown` card which is necessary because
+// our markdown-only editor was using the `card-markdown` card name
+let markdownCard = require('./markdown');
+module.exports = markdownCard;

core/server/lib/mobiledoc/cards/hr.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-hr',
+    name: 'hr',
     type: 'dom',
     render(opts) {
         return opts.env.dom.createElement('hr');

core/server/lib/mobiledoc/cards/html.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    name: 'card-html',
+    name: 'html',
     type: 'dom',
     render(opts) {
         let html = `<div class="kg-card-html">${opts.payload.html}</div>`;

core/server/lib/mobiledoc/cards/image.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-image',
+    name: 'image',
     type: 'dom',
     render(opts) {
         var img = opts.env.dom.createElement('img');

core/server/lib/mobiledoc/cards/markdown.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-markdown',
+    name: 'markdown',
     type: 'dom',
     render: function (opts) {
         var converters = require('../converters'),

commit 763e9e2b335cd1c11175e341f2c267ee50db937b

Fixed missing export of `card-markdown` card and broken tests
no issue
- fixes rendering bug introduced in https://github.com/TryGhost/Ghost/commit/2add938e9642110b92c94542e6b0538c45ae8242
- updates test generators/fixtures to use new card names

core/server/lib/mobiledoc/cards/card-markdown.js
@@ -4,4 +4,4 @@
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./markdown');
 
-module.exports = Object.assign(markdownCard, {name: 'card-markdown'});
+module.exports = Object.assign({}, markdownCard, {name: 'card-markdown'});

core/test/unit/lib/mobiledoc/converters/mobiledoc-converter_spec.js
@@ -6,16 +6,16 @@ describe('Convert mobiledoc to HTML ', function () {
         version: '0.3.1',
         atoms: [],
         cards: [
-            ['card-markdown',
+            ['markdown',
                 {
                     pos: 'top',
-                    card_name: 'card-markdown',
+                    card_name: 'markdown',
                     markdown: '#heading\n\n- list one\n- list two\n- list three'
                 }
             ],
-            ['card-html', {
+            ['html', {
                 pos: 'top',
-                card_name: 'card-html',
+                card_name: 'html',
                 html: '<p>HTML CARD</p>'
             }]
         ],

core/test/utils/fixtures/data-generator.js
@@ -11,8 +11,8 @@ DataGenerator.markdownToMobiledoc = function markdownToMobiledoc(content) {
         markups: [],
         atoms: [],
         cards: [
-            ['card-markdown', {
-                cardName: 'card-markdown',
+            ['markdown', {
+                cardName: 'markdown',
                 markdown: content || ''
             }]
         ],