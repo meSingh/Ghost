meSingh/Ghost

commit 6a6440803733fff16596bae262e8d3d37876cb6c

Imported nested tags by foreign key
no issue

- replace logic for preparing nested tags
- if you have nested tags in your file, we won't update or update the target tag
- we simply would like to add the relationship to the database
- use same approach as base class
  - add `posts_tags` to target post model
  - update identifiers
  - insert relation by foreign key `tag_id`
- bump bookshelf-relations to 0.1.10

core/server/data/importer/importers/data/base.js
@@ -34,12 +34,21 @@ class Base {
         this.importedDataToReturn = [];
         this.importedData = [];
 
-        this.options.requiredImportedData = ['users'];
-        this.options.requiredExistingData = ['users'];
-
         this.requiredFromFile = {};
         this.requiredImportedData = {};
         this.requiredExistingData = {};
+
+        if (!this.options.requiredImportedData) {
+            this.options.requiredImportedData = ['users'];
+        } else {
+            this.options.requiredImportedData.push('users');
+        }
+
+        if (!this.options.requiredExistingData) {
+            this.options.requiredExistingData = ['users'];
+        } else {
+            this.options.requiredExistingData.push('users');
+        }
 
         if (!this.options.requiredFromFile) {
             this.options.requiredFromFile = ['users'];

core/server/data/importer/importers/data/posts.js
@@ -11,7 +11,9 @@ class PostsImporter extends BaseImporter {
         super(allDataFromFile, {
             modelName: 'Post',
             dataKeyToImport: 'posts',
-            requiredFromFile: ['tags', 'posts_tags']
+            requiredFromFile: ['posts', 'tags', 'posts_tags'],
+            requiredImportedData: ['tags'],
+            requiredExistingData: ['tags']
         });
 
         this.legacyKeys = {
@@ -28,76 +30,93 @@ class PostsImporter extends BaseImporter {
     }
 
     /**
-     * We don't have to worry about existing tag id's.
-     * e.g. you import a tag, which exists (doesn't get imported)
-     * ...because we add tags by unique name.
+     * Naive function to attach related tags.
+     * Target tags should not be created. We add the relation by foreign key.
      */
-    addTagsToPosts() {
-        let postTags = this.requiredFromFile.posts_tags,
-            postsWithTags = new Map(),
-            duplicatedTagsPerPost = {},
-            tagsToAttach = [],
-            foundOriginalTag;
+    addNestedRelations() {
+        this.requiredFromFile.posts_tags = _.orderBy(this.requiredFromFile.posts_tags, ['post_id', 'sort_order'], ['asc', 'asc']);
 
-        postTags = _.orderBy(postTags, ['post_id', 'sort_order'], ['asc', 'asc']);
-
-        _.each(postTags, (postTag) => {
-            if (!postsWithTags.get(postTag.post_id)) {
-                postsWithTags.set(postTag.post_id, []);
+        /**
+         * {post_id: 1, tag_id: 2}
+         */
+        _.each(this.requiredFromFile.posts_tags, (postTagRelation) => {
+            if (!postTagRelation.post_id) {
+                return;
             }
 
-            if (postsWithTags.get(postTag.post_id).indexOf(postTag.tag_id) !== -1) {
-                if (!duplicatedTagsPerPost.hasOwnProperty(postTag.post_id)) {
-                    duplicatedTagsPerPost[postTag.post_id] = [];
+            let postToImport = _.find(this.dataToImport, {id: postTagRelation.post_id});
+
+            // CASE: we won't import a relation when the target post does not exist
+            if (!postToImport) {
+                return;
+            }
+
+            if (!postToImport.tags || !_.isArray(postToImport.tags)) {
+                postToImport.tags = [];
+            }
+
+            // CASE: duplicate relation?
+            if (!_.find(postToImport.tags, {tag_id: postTagRelation.tag_id})) {
+                postToImport.tags.push({
+                    tag_id: postTagRelation.tag_id
+                });
+            }
+        });
+    }
+
+    /**
+     * Replace all `tag_id` references.
+     */
+    replaceIdentifiers() {
+        /**
+         * {post_id: 1, tag_id: 2}
+         */
+        _.each(this.dataToImport, (postToImport, postIndex) => {
+            if (!postToImport.tags || !postToImport.tags.length) {
+                return;
+            }
+
+            let indexesToRemove = [];
+            _.each(postToImport.tags, (tag, tagIndex) => {
+                let tagInFile = _.find(this.requiredFromFile.tags, {id: tag.tag_id});
+
+                if (!tagInFile) {
+                    let existingTag = _.find(this.requiredExistingData.tags, {id: tag.tag_id});
+
+                    // CASE: tag is not in file, tag is not in db
+                    if (!existingTag) {
+                        indexesToRemove.push(tagIndex);
+                        return;
+                    } else {
+                        this.dataToImport[postIndex].tags[tagIndex].tag_id = existingTag.id;
+                        return;
+                    }
                 }
 
-                duplicatedTagsPerPost[postTag.post_id].push(postTag.tag_id);
-            }
+                // CASE: search through imported data
+                let importedTag = _.find(this.requiredImportedData.tags, {slug: tagInFile.slug});
 
-            postsWithTags.get(postTag.post_id).push(postTag.tag_id);
-        });
-
-        postsWithTags.forEach((tagIds, postId) => {
-            tagsToAttach = [];
-
-            _.each(tagIds, (tagId) => {
-                foundOriginalTag = _.find(this.requiredFromFile.tags, {id: tagId});
-
-                if (!foundOriginalTag) {
+                if (importedTag) {
+                    this.dataToImport[postIndex].tags[tagIndex].tag_id = importedTag.id;
                     return;
                 }
 
-                tagsToAttach.push(foundOriginalTag);
+                // CASE: search through existing data
+                let existingTag = _.find(this.requiredExistingData.tags, {slug: tagInFile.slug});
+
+                if (existingTag) {
+                    this.dataToImport[postIndex].tags[tagIndex].tag_id = existingTag.id;
+                } else {
+                    indexesToRemove.push(tagIndex);
+                }
             });
 
-            _.each(tagsToAttach, (tag) => {
-                _.each(this.dataToImport, (obj) => {
-                    if (obj.id === postId) {
-                        if (!_.isArray(obj.tags)) {
-                            obj.tags = [];
-                        }
-
-                        if (duplicatedTagsPerPost.hasOwnProperty(postId) && duplicatedTagsPerPost[postId].length) {
-                            this.problems.push({
-                                message: 'Detected duplicated tags for: ' + obj.title || obj.slug,
-                                help: this.modelName,
-                                context: JSON.stringify({
-                                    tags: _.map(_.filter(this.requiredFromFile.tags, (tag) => {
-                                        return _.indexOf(duplicatedTagsPerPost[postId], tag.id) !== -1;
-                                    }), (value) => {
-                                        return value.slug || value.name;
-                                    })
-                                })
-                            });
-                        }
-
-                        obj.tags.push({
-                            name: tag.name
-                        });
-                    }
-                });
-            });
+            this.dataToImport[postIndex].tags = _.filter(this.dataToImport[postIndex].tags, ((tag, index) => {
+                return indexesToRemove.indexOf(index) === -1;
+            }));
         });
+
+        return super.replaceIdentifiers();
     }
 
     beforeImport() {
@@ -105,7 +124,7 @@ class PostsImporter extends BaseImporter {
         let mobileDocContent;
 
         this.sanitizeAttributes();
-        this.addTagsToPosts();
+        this.addNestedRelations();
 
         // Remove legacy field language
         this.dataToImport = _.filter(this.dataToImport, (data) => {

core/server/data/importer/importers/data/tags.js
@@ -19,6 +19,13 @@ class TagsImporter extends BaseImporter {
         };
     }
 
+    fetchExisting(modelOptions) {
+        return models.Tag.findAll(_.merge({columns: ['id', 'slug']}, modelOptions))
+            .then((existingData) => {
+                this.existingData = existingData.toJSON();
+            });
+    }
+
     beforeImport() {
         debug('beforeImport');
         return super.beforeImport();
@@ -28,7 +35,7 @@ class TagsImporter extends BaseImporter {
      * Find tag before adding.
      * Background:
      *   - the tag model is smart enough to regenerate unique fields
-     *   - so if you import a tag name "test" and the same tag name exists, it would add "test-2"
+     *   - so if you import a tag slug "test" and the same tag slug exists, it would add "test-2"
      *   - that's why we add a protection here to first find the tag
      *
      * @TODO: Add a flag to the base implementation e.g. `fetchBeforeAdd`
@@ -56,6 +63,12 @@ class TagsImporter extends BaseImporter {
                             if (importOptions.returnImportedData) {
                                 this.importedDataToReturn.push(importedModel.toJSON());
                             }
+
+                            // for identifier lookup
+                            this.importedData.push({
+                                id: importedModel.id,
+                                slug: importedModel.get('slug')
+                            });
 
                             return importedModel;
                         })

core/server/models/post.js
@@ -211,7 +211,7 @@ Post = ghostBookshelf.Model.extend({
             //  and deduplicate upper/lowercase tags
             _.each(this.get('tags'), function each(item) {
                 for (i = 0; i < tagsToSave.length; i = i + 1) {
-                    if (tagsToSave[i].name.toLocaleLowerCase() === item.name.toLocaleLowerCase()) {
+                    if (tagsToSave[i].name && item.name && tagsToSave[i].name.toLocaleLowerCase() === item.name.toLocaleLowerCase()) {
                         return;
                     }
                 }

core/test/integration/data/importer/importers/data_spec.js
@@ -427,8 +427,10 @@ describe('Import', function () {
                 // NOTE: a duplicated tag.slug is a warning
                 response[0].errorType.should.equal('ValidationError');
                 response[0].message.should.eql('Value in [tags.name] cannot be blank.');
+
                 response[1].errorType.should.equal('ValidationError');
                 response[1].message.should.eql('Value in [posts.title] cannot be blank.');
+
                 response[2].errorType.should.equal('ValidationError');
                 response[2].message.should.eql('Value in [settings.key] cannot be blank.');
 
@@ -480,6 +482,7 @@ describe('Import', function () {
 
                 importedData.problems[2].message.should.eql('Entry was imported, but we were not able to ' +
                     'resolve the following user references: author_id, published_by. The user does not exist, fallback to owner user.');
+                importedData.problems[2].help.should.eql('Post');
 
                 // Grab the data from tables
                 return Promise.all([
@@ -527,11 +530,9 @@ describe('Import', function () {
             }).then(function () {
                 done(new Error('Allowed import of invalid tags data'));
             }).catch(function (response) {
-                response.length.should.equal(2);
+                response.length.should.equal(1);
                 response[0].errorType.should.equal('ValidationError');
                 response[0].message.should.eql('Value in [tags.name] cannot be blank.');
-                response[1].errorType.should.equal('ValidationError');
-                response[1].message.should.eql('Value in [tags.name] cannot be blank.');
                 done();
             }).catch(done);
         });

commit aab389fe204fc5afbe6fab02ffc240c5238fcdf8

🐛 Fixed import for tag without slugs that belongs to a post (#10917)
closes #10785

- The behavior for tags will now be similar to posts' one described in the docs
- "The only strictly required field when importing posts is the title. Ghost will automatically generate slugs and set every other field to the default or empty."
- The breaking change was introduced with: https://github.com/TryGhost/Ghost/commit/6a6440803733fff16596bae262e8d3d37876cb6c#diff-e712df50c0dc7cf33746eeff0564003cR97 (assumed there's always slug in the imported object which is not true)
- Added originalIdMap to the importer base class to track id
substitution so it can be used when dealing with relational resource
updates
- Removed explicit use of 'this.stripProperties(['id']);' in
beforeImport of base class because we need to assign and remove the id
property in the same place to track this change
- Only calling 'this.stripProperties(['id']);' in
settings/trusted_domain imports as the method won't be called otherwise
- Expanded regression tests with new supported import case

core/server/data/importer/importers/data/base.js
@@ -21,6 +21,7 @@ class Base {
 
         this.dataKeyToImport = options.dataKeyToImport;
         this.dataToImport = _.cloneDeep(allDataFromFile[this.dataKeyToImport] || []);
+        this.originalIdMap = {};
 
         this.importedDataToReturn = [];
         this.importedData = [];
@@ -53,7 +54,7 @@ class Base {
     }
 
     /**
-     * Never ever import these attributes!
+     * Strips attributes of the object
      */
     stripProperties(properties) {
         _.each(this.dataToImport, (obj) => {
@@ -86,7 +87,13 @@ class Base {
 
     generateIdentifier() {
         _.each(this.dataToImport, (obj) => {
-            obj.id = ObjectId.generate();
+            const newId = ObjectId.generate();
+
+            if (obj.id) {
+                this.originalIdMap[newId] = obj.id;
+            }
+
+            obj.id = newId;
         });
     }
 
@@ -95,7 +102,6 @@ class Base {
     }
 
     beforeImport() {
-        this.stripProperties(['id']);
         this.sanitizeValues();
         this.generateIdentifier();
         return Promise.resolve();
@@ -317,6 +323,7 @@ class Base {
                         // for identifier lookup
                         this.importedData.push({
                             id: importedModel.id,
+                            originalId: this.originalIdMap[importedModel.id],
                             slug: importedModel.get('slug'),
                             originalSlug: obj.slug,
                             email: importedModel.get('email')

core/server/data/importer/importers/data/posts.js
@@ -100,7 +100,11 @@ class PostsImporter extends BaseImporter {
 
                 // CASE: search through imported data.
                 // EDGE CASE: uppercase tag slug was imported and auto modified
-                let importedObject = _.find(this.requiredImportedData[tableName], {originalSlug: objectInFile.slug});
+                let importedObject = null;
+
+                if (objectInFile.id) {
+                    importedObject = _.find(this.requiredImportedData[tableName], {originalId: objectInFile.id});
+                }
 
                 if (importedObject) {
                     this.dataToImport[postIndex][targetProperty][index].id = importedObject.id;

core/server/data/importer/importers/data/settings.js
@@ -109,6 +109,7 @@ class SettingsImporter extends BaseImporter {
     }
 
     generateIdentifier() {
+        this.stripProperties(['id']);
         return Promise.resolve();
     }
 

core/server/data/importer/importers/data/tags.js
@@ -58,6 +58,7 @@ class TagsImporter extends BaseImporter {
                             // for identifier lookup
                             this.importedData.push({
                                 id: importedModel.id,
+                                originalId: this.originalIdMap[importedModel.id],
                                 slug: importedModel.get('slug'),
                                 originalSlug: obj.slug
                             });

core/server/data/importer/importers/data/trusted-domains.js
@@ -75,6 +75,7 @@ class TrustedDomainsImporter extends BaseImporter {
     }
 
     generateIdentifier() {
+        this.stripProperties(['id']);
         return Promise.resolve();
     }
 

core/test/regression/importer/importer_spec.js
@@ -809,6 +809,55 @@ describe('Integration: Importer', function () {
                 });
         });
 
+        it('can handle related tags with missing optional fields', function () {
+            const exportData = exportedLatestBody().db[0];
+
+            exportData.data.posts[0] = testUtils.DataGenerator.forKnex.createPost();
+
+            // NOTE: not including slug, description etc. fields as the only required field
+            // to handle the import of tags is 'name'
+            exportData.data.tags[0] = {
+                id: ObjectId.generate(),
+                name: 'first tag'
+            };
+            exportData.data.tags[1] = {
+                id: ObjectId.generate(),
+                name: 'second tag'
+            };
+            exportData.data.tags[2] = {
+                id: ObjectId.generate(),
+                name: 'third tag'
+            };
+
+            exportData.data.posts_tags = [
+                testUtils.DataGenerator.forKnex.createPostsTags(exportData.data.posts[0].id, exportData.data.tags[0].id),
+                testUtils.DataGenerator.forKnex.createPostsTags(exportData.data.posts[0].id, exportData.data.tags[1].id),
+                testUtils.DataGenerator.forKnex.createPostsTags(exportData.data.posts[0].id, exportData.data.tags[2].id)
+            ];
+
+            return dataImporter.doImport(exportData, importOptions)
+                .then(function (imported) {
+                    imported.problems.length.should.eql(0);
+
+                    return Promise.all([
+                        models.Tag.findPage(Object.assign({order: 'slug ASC'}, testUtils.context.internal)),
+                        models.Post.findPage(Object.assign({withRelated: ['tags']}, testUtils.context.internal))
+                    ]);
+                }).then(function (result) {
+                    const tags = result[0].data.map(model => model.toJSON());
+                    const posts = result[1].data.map(model => model.toJSON());
+
+                    posts.length.should.eql(1);
+                    tags.length.should.eql(3);
+
+                    posts[0].tags.length.should.eql(3);
+                    tags[0].name.should.eql('first tag');
+                    tags[0].slug.should.eql('first-tag');
+                    tags[1].name.should.eql('second tag');
+                    tags[2].name.should.eql('third tag');
+                });
+        });
+
         it('can handle uppercase tags', function () {
             const exportData = exportedLatestBody().db[0];
 